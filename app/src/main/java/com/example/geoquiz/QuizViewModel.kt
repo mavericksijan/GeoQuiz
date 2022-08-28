f1f9cf93ee79b7b47dbdf3619b600996f9d1dc5d
mavericksijan <101683965+mavericksijan@users.noreply.github.com>
2022-08-28 14:18:38 +1000

    cheatActivity created

---
+++ app/src/main/java/com/example/geoquiz/QuizViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.geoquiz
+
+import android.util.Log
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+
+private const val TAG = "QuizViewModel"
+const val IS_CHEATER_KEY = "IS_CHEATER_KEY"
+class QuizViewModel(private val savedStateHandle: SavedStateHandle): ViewModel() {
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_canal, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+
+    var isCheater: Boolean
+        get() = savedStateHandle.get(IS_CHEATER_KEY) ?: false
+        set(value) = savedStateHandle.set(IS_CHEATER_KEY, value)
+
+    private var currentIndex: Int= 0
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToNext(){
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
+
+
+}
